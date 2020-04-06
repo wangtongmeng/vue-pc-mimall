01da12be79bf5ea0681a9b3367a1eb1b77b60d7e
wangtongmeng <tongmengwang@163.com>
2020-04-06 09:13:26 +0800

    目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import jsonp from 'jsonp'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   mounted() {
-    const url = 'api/pay/cartorder'
-    jsonp(url, {
-      name: 'jsonpcallback'
-    }, function (err, data) {
-      console.log(1);
-      
-      console.log(err, data)
-    })
   }
 }
 </script>
