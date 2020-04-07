c171109aeba93fc152e97ca9acf81efcdaba5447
wangtongmeng <tongmengwang@163.com>
2020-04-07 15:05:23 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 根据前端的跨域方式做调整 例如，代理的方式： /a/b /api/a/b => /a/b
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+// 接口错误拦截
+axios.interceptors.response.use(function (response) {
+  const res = response.data
+  if (res.status == 0) { // 请求成功
+    return res.data
+  } else if (res.status == 10) { // 未登录
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+
+Vue.use(VueAxios, axios)
+Vue.config.productionTip = false // 生产环境的提示，默认是false
 
 new Vue({
   router,
